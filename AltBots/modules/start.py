1516ad57e4b992a2dfc30e1e2bafe0d2ea4514f7
Adarsh9414 <168310122+Adarsh9414@users.noreply.github.com>
2024-05-29 14:26:18 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -5,12 +5,12 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_BUTTON = [
     [
-        Button.inline(" 𝐌ᴜsɪᴄ ", "https://t.me/RUHI_X_MUSICBOT"),
-        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/its_innocent_boy_2926")
+        Button.inline(" Coder ", "https://t.me/daddy_deviLl_mere"),
+        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/daddy_deviLl_mere")
     ],
     [
-        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/THE_FUCKING_BOT_2926"),
-        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/THE_FUCKER_BOTS_2926")
+        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/Devilspambotrepo"),
+        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/Devilspambotrepo")
     ],
     [
         Button.url("𝐑ᴇᴘᴏ ", "github.com/nakuldkdhacker0026/RAID-SPAMBOT"),
@@ -36,7 +36,7 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [👑NAKUL👑](https://t.me/its_innocent_boy_2926)**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [👑[꧁༒★ཌﮩ٨ـﮩﮩ٨ـ ❤️ﮩ٨ـﮩﮩ٨ـ❥ DEVIL VIKRAM ❤️ﮩ٨ـﮩﮩ٨ـﮩ٨★༒꧂](👑](https://t.me/daddy_deviLl_mere)**\n\n"
         TEXT += f"» **ᴅᴇᴀᴅ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
